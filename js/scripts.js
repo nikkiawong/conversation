ca11abbecb0ee29f9984c10f67982cea545ee2f5
Kendra Kelly and Nikki Wong <nikki@>
2018-08-07 14:38:11 -0700

    add a button linking to an image after the button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// var fidoImg = new Image();
+// fidoImg.src = "<img src=\"img/fido.jpg\">"
+
 $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>MEOW!</li>");
@@ -8,4 +11,12 @@ $(document).ready(function() {
     $("ul#dog").prepend("<li>WOOF!</li>");
     $("ul#cat").prepend("<li>MEOW!</li>");
   });
+
+  $("button#fidobtn").click(function() {
+    $("button#fidobtn").after("<img src=\"img/fido.jpg\">");
+  });
+
+  // $("button#hideFidoBtn").click(function() {
+  //   $("button#hideFidoBtn").remove("<img src=\"img/fido.jpg\">");
+  // });
 });
